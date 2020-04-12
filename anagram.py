e1fe1b593ce89e514fa85de1462ab8d20c787654
gopigopalb <56151751+gopigopalb@users.noreply.github.com>
2020-04-12 10:45:20 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,11 @@
+def check(s1, s2): 
+      
+    # the sorted strings are checked  
+    if(sorted(s1)== sorted(s2)): 
+        print("The strings are anagrams.")  
+    else: 
+        print("The strings aren't anagrams.")          
+          
+s1 ="listen"
+s2 ="silent" 
+check(s1, s2) 
